89f6b7181e5979ed0dd4ea7ad3501fed497b503b
kendax <yabuntupeta54@gmail.com>
2024-03-24 23:33:03 +0300

    second commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,10 @@
 import { AppProps } from "next/app";
 import Head from "next/head";
 
-const APP_NAME = "PWA App";
-const APP_DEFAULT_TITLE = "My Awesome PWA App";
-const APP_TITLE_TEMPLATE = "%s - PWA App";
-const APP_DESCRIPTION = "Best PWA app in the world!";
+const APP_NAME = "Simmer";
+const APP_DEFAULT_TITLE = "What's simmering!";
+const APP_TITLE_TEMPLATE = "%s - Simmer";
+const APP_DESCRIPTION = "Ready in a few";
 
 export const metadata = {
   applicationName: APP_NAME,
@@ -58,7 +58,7 @@ export default function App({ Component, pageProps }) {
           <meta name="format-detection" content="telephone=no" />
           <meta name="mobile-web-app-capable" content="yes" />
           <meta name="theme-color" content="#FFFFFF" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
           <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
           <link rel="manifest" href="/manifest.json" />
           <link rel="shortcut icon" href="/favicon.ico" />
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -32,7 +32,9 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        </Head>
         <body>
           <Main />
           <NextScript />
